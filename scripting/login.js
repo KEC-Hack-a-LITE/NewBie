ad9841d1d449d2a41b2d11d0c4fb621ed85a32ef
Dhirendra Singh Dhami <dhirendraxd@gmail.com>
2024-02-17 00:19:10 +0545

    login page features added

--- scripting/login.js
+++ scripting/login.js
@@ -1,4 +1,13 @@
 function redirectTodashboard(event) {
-    event.preventDefault();
-     window.location.href = " dashboard.html";
-      }
+    event.preventDefault(); // Prevent form submission
+    var ageSelect = document.getElementById('age');
+    var selectedAge = ageSelect.value;
+    if (selectedAge === '') {
+        alert("Please select your age range.");
+    } else if (selectedAge === '18-30') {
+        alert("Sorry, you must be at least 18 years old to login.");
+    } else {
+        
+        window.location.href = "dashboard.php";
+    }
+}
